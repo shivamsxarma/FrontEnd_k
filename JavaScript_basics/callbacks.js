5c4988c9472157bd1cef0b81ea57d0a43ff2a5f4
shivam sharma <shivamsxarma@gmail.com>
2026-03-26 12:20:28 +0530

    new update

---
+++ JavaScript_basics/callbacks.js
@@ -0,0 +1,25 @@
+// function outer (){
+//     // console.log(a)
+//     return function inner (){
+//         let a = 1;
+
+//     }
+// }
+
+// outer()
+// const payment = function(){
+//     console.log("hlww")
+// }
+// let payment1=payment;
+// payment ()
+
+// function multiplication (value ){
+// return function (num){
+//     return num * value;
+    
+// };
+// } ;
+// let ans = multiplication(4)(5)
+// console.log(num)                
+
+//callback : function accepcting another function as an argument  are called an call back function
